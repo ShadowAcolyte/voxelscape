24cb4b88f80138719a0f6c86e36f670e0b71981c
Samvrit <samvrit8703@gmail.com>
2022-01-27 18:22:45 +0530

    config utility

---
+++ src/config.cpp
@@ -0,0 +1,8 @@
+#include "config.h"
+
+int config::screen_width = 1280;
+int config::screen_height = 720;
+const char* config::screen_title = "Voxelscape";
+
+float config::fov = 60.0f;
+float config::mouse_senstivity = 0.1f;
---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef GAIA_CONFIG_H
+#define GAIA_CONFIG_H
+
+namespace config
+{
+    extern int screen_width, screen_height;
+    extern const char* screen_title;
+
+    extern float fov;
+    extern float mouse_senstivity;
+}
+
+#endif // GAIA_CONFIG_H
