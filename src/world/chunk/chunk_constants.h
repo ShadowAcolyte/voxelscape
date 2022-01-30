f0179e72794b15ac77c4a1fd39df8e66b9105892
Samvrit <samvrit8703@gmail.com>
2022-01-30 22:11:24 +0530

    Setup website

--- src/world/chunk/chunk_constants.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAIA_CHUNK_CONSTANTS_H
-#define GAIA_CHUNK_CONSTANTS_H
-
-constexpr int CHUNK_SIZE = 16;
-constexpr int CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE;
-constexpr int CHUNK_VOLUME = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
-
-#endif
--- src/world/world.h
+++
@@ -1,6 +0,0 @@
-#ifndef VOX_WORLD_H
-#define VOX_WORLD_H
-
-
-
-#endif // !VOX_WORLD_H
