db9ff96b88aa11883db4802ee699355ff581a50f
Samvrit <samvrit8703@gmail.com>
2022-10-06 19:53:29 +0530

    Code refactor

--- src/engine/log.cpp
+++ src/engine/log.cpp
@@ -9,15 +9,15 @@
 
 #include "log.h"
 
-static std::vector<spdlog::sink_ptr> __sinks;
-static std::shared_ptr<spdlog::logger> __core_logger;
+static std::vector<spdlog::sink_ptr> sinks;
+static std::shared_ptr<spdlog::logger> coreLogger;
 
 void Logger::Init() {
-    auto _stdout_sink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
+    auto stdoutSink = std::make_shared<spdlog::sinks::stdout_color_sink_mt>();
 #ifdef _WIN32
-    _stdout_sink->set_color(spdlog::level::info, 0b1010);
+    stdoutSink->set_color(spdlog::level::info, 0b1010);
 #else
-    _stdout_sink->set_color(spdlog::level::info, "\033[92m");
+    stdoutSink->set_color(spdlog::level::info, "\033[92m");
 #endif // _WIN32
 
     std::stringstream log_file_path;
@@ -26,9 +26,9 @@ void Logger::Init() {
     log_file_path << std::put_time(std::localtime(&t), "%d-%m-%Y-%H-%M-%S");
     log_file_path << ".txt";
 
-    __sinks.push_back(_stdout_sink);
-    __sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file_path.str(), 1024 * 1024 * 5, 3));
-    __core_logger = std::make_shared<spdlog::logger>("logger_core", begin(__sinks), end(__sinks));
-    __core_logger->set_pattern("[%H:%M:%S] [%l] %^%v%$");
-    spdlog::register_logger(__core_logger);
+    sinks.push_back(stdoutSink);
+    sinks.push_back(std::make_shared<spdlog::sinks::rotating_file_sink_mt>(log_file_path.str(), 1024 * 1024 * 5, 3));
+    coreLogger = std::make_shared<spdlog::logger>("logger_core", begin(sinks), end(sinks));
+    coreLogger->set_pattern("[%H:%M:%S] [%l] %^%v%$");
+    spdlog::register_logger(coreLogger);
 }
